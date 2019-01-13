e3d28b6ea3f488d34b0e5d1f04cdce78406f47c4
Thomas Holmes <thomasholmes@outlook.com>
2019-01-13 13:38:42 +0000

    Added multiple article index pages.

--- NewsCrawler/BbcNewsArticleFinderService.cs
+++ NewsCrawler/BbcNewsArticleFinderService.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace NewsCrawler
 {
@@ -12,6 +11,19 @@ namespace NewsCrawler
 
         private readonly string baseUrl = "https://www.bbc.co.uk";
 
+        private readonly string[] indexUrls = new[]
+        {
+            "https://www.bbc.co.uk/news/uk",
+            "https://www.bbc.co.uk/news/politics",
+            "https://www.bbc.co.uk/news/health",
+            "https://www.bbc.co.uk/news/education",
+            "https://www.bbc.co.uk/news/technology",
+            "https://www.bbc.co.uk/news/science_and_environment",
+            "https://www.bbc.co.uk/news/entertainment_and_arts",
+            "https://www.bbc.co.uk/news/stories",
+            "https://www.bbc.co.uk/news/popular/read"
+        };
+
         public BbcNewsArticleFinderService(INewsArticleDeterminationService newsArticleDeterminationService)
         {
             this.newsArticleDeterminationService = newsArticleDeterminationService ?? throw new ArgumentNullException(nameof(newsArticleDeterminationService));
@@ -19,15 +31,21 @@ namespace NewsCrawler
 
         public IEnumerable<string> FindNewsArticles()
         {
-            var url = "https://www.bbc.co.uk/news/uk";
-            var web = new HtmlWeb();
-            var doc = web.Load(url);
+            var docuemntNodes = new List<HtmlNode>();
+            foreach (var indexUrl in indexUrls)
+            {
+                var web = new HtmlWeb();
+                var doc = web.Load(indexUrl);
+                docuemntNodes.Add(doc.DocumentNode);
+            }
 
-            var links = doc.DocumentNode.Descendants()
+            var links = docuemntNodes.SelectMany(n => n.Descendants())
                 .Where(n => n.Name == "a")
                 .Select(n => FindHref(n))
                 .Where(v => newsArticleDeterminationService.IsNewsArticle(v))
-                .Select(v => $"{baseUrl}{v}");
+                .Select(v => $"{baseUrl}{v}")
+                .Distinct()
+                .ToArray();
             return links;
         }
 
