05e57635b2dbbae6827375e089f1ba4726770838
Thomas Holmes <thomasholmes@outlook.com>
2019-02-19 21:59:55 +0000

    Added more Daily Mail index urls.

--- NewsCrawler/DailyMail/DailyMailNewsArticleFinderService.cs
+++ NewsCrawler/DailyMail/DailyMailNewsArticleFinderService.cs
@@ -14,7 +14,37 @@ namespace NewsCrawler.DailyMail
 
         private readonly string[] indexUrls = new[]
         {
-            "https://www.dailymail.co.uk/home/index.html"
+            "https://www.dailymail.co.uk/",
+            "https://www.dailymail.co.uk/home/sitemap.html",
+            "https://www.dailymail.co.uk/home/sitemaparchive/index.html",
+            "https://www.dailymail.co.uk/topics",
+            "https://www.dailymail.co.uk/mobile",
+            "https://www.dailymail.co.uk/screensaver",
+            "https://www.dailymail.co.uk/home/rssMenu.html",
+            "https://www.dailymail.co.uk/ourpapers",
+            "https://www.dailymail.co.uk/home/contactus",
+            "https://www.dailymail.co.uk/home/videoarchive/index.html",
+            "https://www.dailymail.co.uk/home/weather/index.html",
+            "https://www.dailymail.co.uk/motoring/index.html",
+            "https://www.dailymail.co.uk/property/index.html",
+            "https://www.dailymail.co.uk/home/prmts/index.html",
+            "https://www.dailymail.co.uk/home/index.html",
+            "https://www.dailymail.co.uk/news/index.html",
+            "https://www.dailymail.co.uk/ushome/index.html",
+            "https://www.dailymail.co.uk/sport/index.html",
+            "https://www.dailymail.co.uk/tvshowbiz/index.html",
+            "https://www.dailymail.co.uk/auhome/index.html",
+            "https://www.dailymail.co.uk/femail/index.html",
+            "https://www.dailymail.co.uk/health/index.html",
+            "https://www.dailymail.co.uk/sciencetech/index.html",
+            "https://www.dailymail.co.uk/video/index.html",
+            "https://www.dailymail.co.uk/travel/index.html",
+            "https://www.dailymail.co.uk/dailymailtv/index.html",
+            "https://www.dailymail.co.uk/home/latest/index.html",
+            "https://www.dailymail.co.uk/news/worldnews/index.html",
+            "https://www.dailymail.co.uk/home/event/index.html",
+            "https://www.dailymail.co.uk/home/books/index.html",
+            "https://www.dailymail.co.uk/money/index.html",
         };
 
         public DailyMailNewsArticleFinderService(INewsArticleDeterminationService newsArticleDeterminationService)
